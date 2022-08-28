32a48f007e47cd633bb33c7b953282d75b7f9547
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-08-28 19:42:52 +0300

    tt

--- src/Kyrgyzstan.java
+++ src/Kyrgyzstan.java
@@ -1,4 +1,25 @@
 public enum Kyrgyzstan {
-    BATKEN,OSH,JALAL_ABAT,YSSYK_KOL,NARYN,TALAS,CHUI
+    BATKEN("Batken = Aigul gul"),OSH("OSH = Sulaiman TOO"),JALAL_ABAT("Jalal abat = ArlanBab,Sary-Chelek"),
+    YSSYK_KOL("Yssyk kol = Yssyk-Kol"),NARYN("Naryn = Naryn dariasy"),TALAS("Talas = Manas"),
+    CHUI("Chui = Kyrgyzstandyn borboru Bishkek");
+    private String ozgocho;
+
+    public String getOzgocho() {
+        return ozgocho;
+    }
+
+    public void setOzgocho(String ozgocho) {
+        this.ozgocho = ozgocho;
+    }
+
+    Kyrgyzstan(String ozgocho) {
+        this.ozgocho = ozgocho;
+    }
+
+    @Override
+    public String toString() {
+        return "Kyrgyzstan{"+ozgocho +
+                '}';
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Kyrgyzstan kyrgyzstan= Kyrgyzstan.valueOf(scanner.nextLine());
-        switch (kyrgyzstan){
-            case OSH :
-                System.out.println("Sulaiman TOO");
-                break;
-            case BATKEN:
-                System.out.println("Aigul gul");
-                break;
-            case JALAL_ABAT:
-                System.out.println("ArlanBab,Sary-Chelek");
-                break;
-            case NARYN:
-                System.out.println("Naryn dariasy");
-                break;
-            case YSSYK_KOL:
-                System.out.println("Yssyk-Kol");
-                break;
-            case CHUI:
-                System.out.println("Bishkek");
-                break;
-            case TALAS:
-                System.out.println("Manas");
-                break;
+
+        Kyrgyzstan[] kyrgyzstan= {
+             Kyrgyzstan.BATKEN,
+             Kyrgyzstan.OSH,
+             Kyrgyzstan.CHUI,
+             Kyrgyzstan.NARYN,
+             Kyrgyzstan.TALAS,
+             Kyrgyzstan.YSSYK_KOL,
+             Kyrgyzstan.JALAL_ABAT
+        };
+        for (Kyrgyzstan kyrgyz:kyrgyzstan) {
+            System.out.println(kyrgyz.getOzgocho());
+        }
+
+
         }
 
     }
-}
